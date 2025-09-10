0db193b2c42ba5d745aaa6c50f705b14b1a22c55
gitond <boti.ortutay@gmail.com>
2025-09-10 17:46:23 +0300

    Portfolio content added to server. Should be formatted right.

--- server/devtools/jsons2homepage.sh
+++ server/devtools/jsons2homepage.sh
@@ -1,1 +1,3 @@
-jq -n --slurpfile picturecard ../page_contents/picturecard.json --slurpfile intro ../page_contents/intro.json '{picturecard: $picturecard[0], intro: $intro[0]}' > ../page_contents/home.json
+# Run this file from server/devtools
+
+jq -n --slurpfile picturecard ../page_contents/picturecard.json --slurpfile intro ../page_contents/intro.json --slurpfile portfolio ../page_contents/portfolio.json '{picturecard: $picturecard[0], intro: $intro[0], portfolio: $portfolio[0]}' > ../page_contents/home.json
